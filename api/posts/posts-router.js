a9602912ded2709c46e8fb796b19d5e6638707dc
juliane1999 <lastjuliane@gmail.com>
2021-08-31 16:24:22 -0700

    passed 16 tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -97,19 +97,19 @@ router.get('/', (req, res) => {
     }
     })
 
-    router.get('./id:/comments', async (req,res) => {
-        try {
-            const post = await Posts.findById(req.params.id)
-            if(!post) {
-                res.status(404).json({
-                    message: "The post with the specified ID does not exist"})
-            } else {
-                await Posts.findPostComments(req.params.id)
-                res.json(post)
-            }
-        } catch (err) {
-            res.status(500).json({ message: "The comments information could not be retrieved" })
-        }
+    router.get('/:id/comments', (req,res) => {
+        Posts.findCommentById(req.params.id)
+            .then(comment => {
+                if(!comment){
+                    res.status(404).json({ message: "The post with the specified ID does not exist" })
+                } else {
+                    res.status(200).json(comment)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({message: "The comments information could not be retrieved"})
+            })
     })
 
   
