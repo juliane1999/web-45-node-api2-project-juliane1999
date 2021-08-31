ad28d40a40f10d986402b360053064369ed4f20d
juliane1999 <lastjuliane@gmail.com>
2021-08-31 16:47:52 -0700

    passed all tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -97,20 +97,41 @@ router.get('/', (req, res) => {
     }
     })
 
-    router.get('/:id/comments', (req,res) => {
+    // router.get('/:id/comments', (req,res) => {
+    //     Posts.findCommentById(req.params.id)
+    //         .then(comment => {
+    //             if(!comment){
+    //                 res.status(404).json({ message: "The post with the specified ID does not exist" })
+    //             } else {
+    //                 res.status(200).json(comment)
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //             res.status(500).json({message: "The comments information could not be retrieved"})
+    //         })
+    // })
+
+    router.get('/:id/comments', (req,res)=> {
         Posts.findCommentById(req.params.id)
-            .then(comment => {
-                if(!comment){
-                    res.status(404).json({ message: "The post with the specified ID does not exist" })
+            .then(post => {
+                if(!post) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"})
                 } else {
-                    res.status(200).json(comment)
+                    return Posts.findPostComments(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
                 }
             })
             .catch(err => {
                 console.log(err)
                 res.status(500).json({message: "The comments information could not be retrieved"})
             })
-    })
+        })
 
   
   
