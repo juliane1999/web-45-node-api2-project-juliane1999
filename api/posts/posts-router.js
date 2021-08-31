34fdc16a609b0996b212b06248dcb8f826300e47
juliane1999 <lastjuliane@gmail.com>
2021-08-31 16:20:20 -0700

    final commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -92,10 +92,26 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             console.log(err)
-            res.status(500).json({ message: "There was an error while saving the post to the database"})
+            res.status(500).json({ message: "The post could not be removed" })
         })
     }
     })
 
+    router.get('./id:/comments', async (req,res) => {
+        try {
+            const post = await Posts.findById(req.params.id)
+            if(!post) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"})
+            } else {
+                await Posts.findPostComments(req.params.id)
+                res.json(post)
+            }
+        } catch (err) {
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        }
+    })
+
+  
   
   module.exports = router
